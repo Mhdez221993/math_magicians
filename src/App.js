579d006910a0238e7c67a256303885c1ca47c763
Moises <mhdezcoronado@gmail.com>
2021-08-10 23:51:30 +0200

    Delete all the boilerplate from CRA (text, images, styles)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import Typography from '@material-ui/core/Typography';
-import './App.css';
-import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
 
         <Typography variant="h2" component="h2" data-test="heading">
           Math Magicians
