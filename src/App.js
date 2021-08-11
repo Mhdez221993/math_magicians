e4b3b38a0d46f91bec692e9dbf9a6f64ec802647
Moises <mhdezcoronado@gmail.com>
2021-08-11 09:14:35 +0200

    Update App to class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
