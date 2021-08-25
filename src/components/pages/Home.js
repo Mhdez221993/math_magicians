f738e3793672d003edc9822e1723b31397de1746
Moises <mhdezcoronado@gmail.com>
2021-08-25 14:51:28 +0200

    Create Home & Quotes compones

---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="description">
+    <h2>Welcome to the Math Magicians App</h2>
+    <p>
+      Calculator provides simple and advanced mathematical functions in a beautifully designed app.
+      Perform basic calculations such as addition, subtraction, multiplication, and division
+      Do scientific operations such as trigonometric, logarithmic, and exponential functions
+    </p>
+  </div>
+);
+export default Home;
