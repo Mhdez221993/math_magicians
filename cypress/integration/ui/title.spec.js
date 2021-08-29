abb03d645896d996aaf404ed32e9fbc65a232938
Moises <mhdezcoronado@gmail.com>
2021-08-29 14:05:34 +0200

    Remove unnessessary files

--- cypress/integration/ui/title.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Bookish application', () => {
-  it('Visits the bookish', () => {
-    cy.visit('http://localhost:3000/');
-    cy.get('h2[data-test="heading"]').contains('Magicians');
-  });
-});
