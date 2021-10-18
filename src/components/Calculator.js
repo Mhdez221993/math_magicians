1be48de92e1dc7ae4877116c21c82b1cde67b7e0
Moises <mhdezcoronado@gmail.com>
2021-10-18 17:11:53 +0200

    Creatge Math magicians components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,3 +1,21 @@
-const Calculator = () => 'Math Magicians';
+import React, { Component } from 'react';
+import Buttons from './Buttons';
+import Screen from './Screen';
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  render() {
+    return (
+      <div className="Calculator">
+        <Screen />
+        <Buttons />
+      </div>
+    );
+  }
+}
 
 export default Calculator;
